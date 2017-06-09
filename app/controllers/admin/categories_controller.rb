c6984086c352fb8ccc2376ec46362ef360cbf830
Bernice Tan <bernice.tann@gmail.com>
2017-06-09 03:05:35 +0000

    Added a new feature with admin security

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTHENTICATION_NAME'], password: ENV['HTTP_BASIC_AUTHENTICATION_PASSWORD']
+
   def index
     @categories = Category.order(id: :desc).all
   end
