e3a241e4d4bba6014e28d7de61fdc8b7f79663ec
Bernice Tan <bernice.tann@gmail.com>
2017-06-13 22:22:41 +0000

    Finished testing for adding an item to cart and seeing it increase by one

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor clicks on the 'Add to Cart' button and increases their cart by one", type: :feature, js: true do
+
+  before :each do
+    @user = User.create!(
+      first_name: 'First',
+      last_name: 'user',
+      email: 'first@user.com',
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "Visitor increases their cart by one" do
+
+    visit '/login'
+
+    within 'form' do
+      fill_in id: 'email', with: @user.email
+      fill_in id: 'password', with: @user.password
+
+      click_button 'Submit'
+    end
+
+    first('article.product').find_link('Add').click
+    expect(page).to have_content 'My Cart (1)'
+    save_screenshot 'cart.png'
+
+  end
 end
