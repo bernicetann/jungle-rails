37850e3e46a55ad5b37186d57a885534ad06abd1
Bernice Tan <bernice.tann@gmail.com>
2017-06-13 20:51:47 +0000

    Finished testing for when users click on the product, they are able to see product details page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates from home page to product detail page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product detail page" do
+
+    visit root_path
+    first('img').click
+    expect(page).to have_css '.product-detail'
+    save_screenshot
+
+  end
+
 end
